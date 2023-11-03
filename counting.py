67d658100b72007c17d546cca3fb0156c6632e05
shviam-2018 <122033083+shviam-2018@users.noreply.github.com>
2023-11-03 18:51:09 +0100

    attempt to fix counting.py

--- counting.py
+++ counting.py
@@ -1,13 +1,13 @@
 
-        
+ham = input("what do you what to count: ")    
+   
 if __name__ == '__main__':
     
-def print_consecutive_numbers(n):
-    for i in range(1, n + 1):
-        print(i, end="")
+    def print_consecutive_numbers(n):
+        for i in range(1, n + 1):
+            print(i, end="")
         
     n = int(input(ham))
     print_consecutive_numbers(n)
 
 
-ham = input("what do you what to count: ")
