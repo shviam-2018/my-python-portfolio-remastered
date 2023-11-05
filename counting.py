bee94a59b8fa65cee476fd70715c974b3acc5cbc
shviam-2018 <122033083+shviam-2018@users.noreply.github.com>
2023-11-05 11:27:00 +0100

    optimize counting.py
    -works
    -prints in a line

--- counting.py
+++ counting.py
@@ -1,13 +1,11 @@
 
-ham = input("what do you what to count: ")    
+ham = int(input("what do you what to count: "))
    
 if __name__ == '__main__':
     
     def print_consecutive_numbers(n):
         for i in range(1, n + 1):
-            print(i, end="")
+            print(i, sep=("-"))
         
-    n = int(input(ham))
+    n = ham
     print_consecutive_numbers(n)
-
-
