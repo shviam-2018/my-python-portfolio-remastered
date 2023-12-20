7c913e311e0d4d8a64fcf24bf2d53ecd5279f1e5
shviam-2018 <122033083+shviam-2018@users.noreply.github.com>
2023-12-20 07:34:56 +0100

    remaster
    changes:
    -better/readable var names.
    -comments to better understand code.

--- Cost split.py
+++ Cost split.py
@@ -1,12 +1,13 @@
 #this asks for the total bill and the ammount of tip you are giving 
 while True:
 
-    TA = input("how much do you what to split: ")
+#this is to ask for the amount and how menay people are going to split it amongst
+    Total_bill = input("how much do you what to split: ")
 
-    SA = input("split among how many people? ")
+    amounnt_to_split = input("split among how many people? ")
 
 #this dose all of the calculation that is needed to find the bill after tip
-    cal = float(TA) / float(SA)
+    cal = float(Total_bill) / float(amounnt_to_split)
 
 #this prints the out of how much the new bill will be 
     print("you'r per head is:",cal)
